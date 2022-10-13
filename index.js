d1e6de47757e616e6654e91409a0209dc5bd172d
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-10-13 09:49:04 -0500

    Added closing tags to html

--- index.js
+++ index.js
@@ -198,6 +198,11 @@ const generateHTML = async () => {
             </div>`
         }
     })
+    html += `</main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>`
     return html
 }
 
